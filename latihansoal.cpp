b194cdbdff81fe7783a78d02eab0029f7baa1166
galang <ahmad.galang.pamungkas24@gmail.com>
2025-05-20 15:58:46 +0700

    first commit

---
+++ latihansoal.cpp
@@ -0,0 +1,41 @@
+// Buatlah program c++ dengan class Barng yang memiliki atribut : namaBarang,
+// KodeBarag
+// program memiliki constructor berparameter
+// untuk mengisi nilai namaBarang dan kodeBarang.
+// serta memiliki fungsi untuk menampilkan informasi Barang.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Barang {
+private:
+    string namaBarang;
+    string kodeBarang;
+
+public:
+    
+    Barang(string nama, string kode) {
+        namaBarang = nama;
+        kodeBarang = kode;
+    }
+
+   
+    void tampilkanInfo() {
+        cout << "Nama Barang : " << namaBarang << endl;
+        cout << "Kode Barang : " << kodeBarang << endl;
+    }
+};
+
+
+int main() {
+    
+    Barang barang1("Hp kang dedi", "Barakmiliter01");
+
+   
+    barang1.tampilkanInfo();
+
+    return 0;
+}
