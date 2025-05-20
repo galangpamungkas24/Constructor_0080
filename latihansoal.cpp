fd60073e26c1eaef923956689585d11de488352b
galang <ahmad.galang.pamungkas24@gmail.com>
2025-05-20 16:03:14 +0700

    deklarasi

--- latihansoal.cpp
+++ latihansoal.cpp
@@ -4,38 +4,6 @@
 // untuk mengisi nilai namaBarang dan kodeBarang.
 // serta memiliki fungsi untuk menampilkan informasi Barang.
 
-
 #include <iostream>
 #include <string>
 using namespace std;
-
-
-class Barang {
-private:
-    string namaBarang;
-    string kodeBarang;
-
-public:
-    
-    Barang(string nama, string kode) {
-        namaBarang = nama;
-        kodeBarang = kode;
-    }
-
-   
-    void tampilkanInfo() {
-        cout << "Nama Barang : " << namaBarang << endl;
-        cout << "Kode Barang : " << kodeBarang << endl;
-    }
-};
-
-
-int main() {
-    
-    Barang barang1("Hp kang dedi", "Barakmiliter01");
-
-   
-    barang1.tampilkanInfo();
-
-    return 0;
-}
